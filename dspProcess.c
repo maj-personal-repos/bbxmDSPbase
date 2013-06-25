7b23441fffc97e8d347989db2542e6463e39deeb
Miguel Alonso Jr <miguel@physiciansglobalnetwork.com>
2013-06-25 16:50:16 -0400

    Testing circular buffer on dsp

--- dspProcess.c
+++ dspProcess.c
@@ -6,13 +6,14 @@
 //#include "buffer.h"
 
 int dspBlockProcess(short *outputBuffer, short *inputBuffer, buffer *xn, int samples){
-	memcpy((char *)outputBuffer, (char *)inputBuffer, 2*samples);
+	//memcpy((char *)outputBuffer, (char *)inputBuffer, 2*samples);
 	
-	//int i;
-	//for (i=0; i < samples; i+=2){
-		//push(xn,(float)inputBuffer[i]);
-		//outputBuffer[i] = (short) pop(xn);
-	//}
+	int i;
+	for (i=0; i < samples; i+=2){
+		push(xn,(float)inputBuffer[i]);
+		outputBuffer[i] = (short) pop(xn);
+		outputBuffer[i+1] = 0;
+	}
 	
 	return DSP_PROCESS_SUCCESS;
 }
