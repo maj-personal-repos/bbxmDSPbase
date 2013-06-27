fde13235959ba0db9c970bf35a1914e1d4e6b958
Miguel Alonso Jr <miguel@physiciansglobalnetwork.com>
2013-06-26 21:50:05 -0400

    Testing filter include

--- dspProcess.c
+++ dspProcess.c
@@ -3,11 +3,22 @@
 #include <string.h>
 
 #include "dspProcess.h"
-#include "fdacoefs.h"
+
+const int BL = 64;
+const short B[64] = {
+        0,      0,      0,      1,      0,     -2,     -5,     -8,     -9,
+       -4,      7,     22,     34,     35,     22,     -1,    -19,    -17,
+        8,     37,     30,    -58,   -239,   -463,   -608,   -502,      9,
+      987,   2343,   3830,   5108,   5847,   5847,   5108,   3830,   2343,
+      987,      9,   -502,   -608,   -463,   -239,    -58,     30,     37,
+        8,    -17,    -19,     -1,     22,     35,     34,     22,      7,
+       -4,     -9,     -8,     -5,     -2,      0,      1,      0,      0,
+        0
+};
 
 // implements an fir filter using fir coefficients defined in fdacoefs.h
 // in fixed-point
-short fir_filter(short *B, buffer *xn){
+short fir_filter(buffer *xn){
 	int j;
 	int yn = 0;
 	// performs the convolution of xn with B
@@ -28,7 +39,7 @@ int dspBlockProcess(short *outputBuffer, short *inputBuffer, buffer *xn, int sam
 		
 		push(xn,inputBuffer[i]); // stores the most recent sample in the circular buffer xn
 		
-		outputBuffer[i] = fir_filter(&B,xn); // filters the input and stores it in the left output channel
+		outputBuffer[i] = fir_filter(xn); // filters the input and stores it in the left output channel
 		
 		outputBuffer[i+1] = 0; // zeros out the right output channel
 	}
